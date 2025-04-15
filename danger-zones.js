a0c7055eb5dc6d739714477ade49f7b921c9a144
Dishantdhyani <139863951+Dishantdhyani@users.noreply.github.com>
2025-04-15 15:49:10 +0530

    Updated SmartSky submodule content

---
+++ danger-zones.js
@@ -0,0 +1,161 @@
+const dangerZone = [
+    {
+      "name": "Bermuda Triangle",
+      "color": "red",
+      "coords": [
+        [25.7617, -80.1918],
+        [18.4655, -66.1057],
+        [32.3078, -64.7505]
+      ]
+    },
+    {
+      "name": "Dragon's Triangle",
+      "color": "darkred",
+      "coords": [
+        [35.6762, 139.6503],  // Tokyo
+        [24.7840, 141.3223],  // Iwo Jima
+        [13.4443, 144.7937]   // Guam
+      ]      
+    },
+    {
+      "name": "Alaska Triangle",
+      "color": "purple",
+      "coords": [
+        [61.2181, -149.9003],
+        [58.3019, -134.4197],
+        [71.2906, -156.7887]
+      ]
+    },
+    {
+      "name": "Michigan Triangle",
+      "color": "blue",
+      "coords": [
+        [44.0, -87.0],
+        [44.0, -85.0],
+        [42.0, -86.0]
+      ]
+    },
+    {
+      "name": "Area 51",
+      "color": "black",
+      "coords": [
+        [37.2653, -115.8],
+        [37.2653, -115.6],
+        [37.1253, -115.6],
+        [37.1253, -115.8]
+      ]
+    },
+    {
+      "name": "Ukraine Conflict Zone",
+      "color": "orangered",
+      "coords": [
+        [50.0, 40.0],
+        [50.0, 32.0],
+        [46.0, 32.0],
+        [46.0, 40.0]
+      ]
+    },
+    {
+      "name": "Afghanistan",
+      "color": "#8B0000",
+      "coords": [
+        [38.0, 75.0],
+        [38.0, 60.0],
+        [29.0, 60.0],
+        [29.0, 75.0]
+      ]
+    },
+    {
+      "name": "Iran Military Zones",
+      "color": "#660000",
+      "coords": [
+        [39.0, 51.0],
+        [39.0, 43.0],
+        [24.0, 43.0],
+        [24.0, 51.0]
+      ]
+    },
+    {
+      "name": "Tibet & Himalayas",
+      "color": "#AA00FF",
+      "coords": [
+        [33.5, 79.0],
+        [33.5, 88.0],
+        [28.0, 88.0],
+        [28.0, 79.0]
+      ]
+    },
+    {
+      "name": "Antarctica Core",
+      "color": "#00CED1",
+      "coords": [
+        [-75.0, 0.0],
+        [-75.0, 60.0],
+        [-90.0, 60.0],
+        [-90.0, 0.0]
+      ]
+    },
+    {
+      "name": "Syria (No-Fly Zones)",
+      "color": "orange",
+      "coords": [
+        [37.5, 41.0],
+        [37.5, 34.0],
+        [32.5, 34.0],
+        [32.5, 41.0]
+      ]
+    },
+    {
+      "name": "Korean Peninsula (North Korea)",
+      "color": "darkgreen",
+      "coords": [
+        [43.0, 131.0],
+        [43.0, 124.0],
+        [38.0, 124.0],
+        [38.0, 131.0]
+      ]
+    },
+    {
+      "name": "Russia (North Caucasus)",
+      "color": "brown",
+      "coords": [
+        [43.0, 42.0],
+        [43.0, 47.0],
+        [40.0, 47.0],
+        [40.0, 42.0]
+      ]
+    },
+    {
+      "name": "Saudi Arabia (Military Zones)",
+      "color": "gold",
+      "coords": [
+        [24.0, 37.0],
+        [24.0, 42.0],
+        [22.0, 42.0],
+        [22.0, 37.0]
+      ]
+    },
+    {
+      "name": "Hawaii (Training Areas)",
+      "color": "yellowgreen",
+      "coords": [
+        [19.5, -155.0],
+        [19.5, -154.0],
+        [20.0, -154.0],
+        [20.0, -155.0]
+      ]
+    },
+    {
+      "name": "South Sudan (Civil War Zones)",
+      "color": "darkviolet",
+      "coords": [
+        [6.0, 30.0],
+        [6.0, 32.0],
+        [4.0, 32.0],
+        [4.0, 30.0]
+      ]
+    }
+  ];
+
+  window.dangerZone = dangerZone;
+  
